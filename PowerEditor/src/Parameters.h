30d20539d75b0b2f3b7c623c8f1840378d0cdab1
J.W. Jagersma <jwjagersma@gmail.com>
2022-10-14 18:23:06 +0200

    Distinguish between "tab size" and "indent size"
    This patch splits the current "tab size" setting into two separate
    settings:
    
    * Tab size   : determines the width of the ASCII `TAB` character.
    * Indent size: determines the tab stop width.
    
    The first option defaults to 8, the latter to 4.  Additionally, "Replace
    by spaces" is renamed to "Use only spaces", to better reflect what it
    actually does.
    
    Pressing the TAB key simply inserts enough space characters to move the
    cursor to the next tab stop.  But if "Use only spaces" is unchecked, and
    the cursor column is divisible by the tab character size, those spaces
    are replaced by tabs.  This doesn't even require any extra code, it is
    a standard Scintilla feature.
    
    For example, with tab size 8 and indent size 4, pressing the TAB key
    on an empty line inserts four spaces.  Pressing it again replaces those
    four spaces with a single `TAB` character.
    
    When tab size and indent size are equal, the behavior is the same as
    before.
    
    Fixes: #10882
    Fixes: #3382

--- PowerEditor/src/Parameters.h
+++ PowerEditor/src/Parameters.h
@@ -719,7 +719,8 @@ struct NppGUI final
 	bool _splitterPos = POS_VERTICAL;
 	int _userDefineDlgStatus = UDD_DOCKED;
 
-	int _tabSize = 4;
+	int _tabSize = 8;
+	int _indentSize = 4;
 	bool _tabReplacedBySpace = false;
 	bool _backspaceUnindent = false;
 
@@ -953,7 +954,9 @@ struct Lang final
 	std::string _pCommentStart;
 	std::string _pCommentEnd;
 
-	int _tabSize = -1;
+	int _tabSize = 0;
+	int _indentSize = 0;
+	bool _useDefaultTab = true;
 	bool _isTabReplacedBySpace = false;
 	bool _isBackspaceUnindent = false;
 
@@ -971,13 +974,17 @@ struct Lang final
 
 	void setTabInfo(int tabInfo)
 	{
-		static constexpr int MASK_ReplaceBySpc = 0x80;
-		static constexpr int MASK_TabSize = 0x7F;
-		if (tabInfo != -1 && tabInfo & MASK_TabSize)
+		static constexpr int MASK_ReplaceBySpc = 0x8000;
+		static constexpr int MASK_IndentSize = 0x00FF;
+		static constexpr int MASK_TabSize = 0x7F00;
+		if (tabInfo != -1 && (tabInfo & MASK_TabSize) && (tabInfo & MASK_IndentSize))
 		{
 			_isTabReplacedBySpace = (tabInfo & MASK_ReplaceBySpc) != 0;
-			_tabSize = tabInfo & MASK_TabSize;
+			_tabSize = (tabInfo & MASK_TabSize) >> 8;
+			_indentSize = tabInfo & MASK_IndentSize;
+			_useDefaultTab = false;
 		}
+		else _useDefaultTab = true;
 	}
 
 	const wchar_t* getDefaultExtList() const {
@@ -997,8 +1004,8 @@ struct Lang final
 
 	int getTabInfo() const
 	{
-		if (_tabSize == -1) return -1;
-		return (_isTabReplacedBySpace?0x80:0x00) | _tabSize;
+		if (_useDefaultTab) return -1;
+		return (_isTabReplacedBySpace ? 0x8000 : 0x0000) | (_tabSize << 8) | _indentSize;
 	}
 };
 
